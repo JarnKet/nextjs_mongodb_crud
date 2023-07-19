b56093d403d4e671bd827b289efad3d9171cd15a
JarnKet <ketsadaphone999@gmail.com>
2023-07-19 18:46:42 +0700

    Display topics list

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ import TopicList from "@/components/TopicList";
 export default function Home() {
   return (
     <section className="flex flex-col gap-y-4">
-      {/* <TopicList /> */}
-      <h1>First Commit</h1>
+      <TopicList />
     </section>
   );
 }
